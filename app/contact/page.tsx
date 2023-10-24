d8519aab3851dd9e506b5179adb67ae4238c89f6
leoDonger <leodonger@icloud.com>
2023-10-23 20:14:09 -0500

    Updated contact page with route Api

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,46 @@
+'use client'
+
+import { useState } from 'react';
 import React from 'react';
+import { FormEvent } from 'react';
+
 
 const Contact = () => {
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  const onSubmit = async (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("Full name: ", fullname);
+    console.log("Email: ", email);
+    console.log("Message: ", message);
+
+    const res = await fetch('api/contact', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fullname,
+        email,
+        message,
+      })
+    });
+
+    const {msg, success} = await res.json();
+    setError(msg);
+    setSuccess(success);
+    if (success){
+      setFullname("");
+      setEmail("");
+      setMessage("");
+    }
+  }
+
 
   return (
     <div className="min-h-[200vh] bg-gradient-to-br from-black via-green-200 to-black text-white">
@@ -13,14 +53,15 @@ const Contact = () => {
         <button className="bg-black text-white px-4 py-2 rounded font-bold">Dashboard</button>
       </nav>
       <main className="h-screen flex flex-col justify-center items-center p-8">
-      <form className="bg-black bg-opacity-50 p-6 rounded-lg shadow-lg text-white">
+      <form className="bg-black bg-opacity-50 p-6 rounded-lg shadow-lg text-white" onSubmit={onSubmit} method='POST'>
           <div className="mb-4">
             <label htmlFor="name" className="block text-lg">Name:</label>
             <input
               type="text"
               id="name"
               name="name"
-              required
+              onChange={e => setFullname(e.target.value)}
+              value={fullname}
               className="w-full px-4 py-2 rounded-lg bg-transparent border border-white text-white focus:outline-none focus:border-green-200"
             />
           </div>
@@ -30,7 +71,8 @@ const Contact = () => {
               type="email"
               id="email"
               name="email"
-              required
+              onChange={e => setEmail(e.target.value)}
+              value={email}
               className="w-full px-4 py-2 rounded-lg bg-transparent border border-white text-white focus:outline-none focus:border-green-200"
             />
           </div>
@@ -40,6 +82,8 @@ const Contact = () => {
               id="message"
               name="message"
               className="w-full px-4 py-2 rounded-lg bg-transparent border border-white text-white focus:outline-none focus:border-green-200"
+              onChange={e => setMessage(e.target.value)}
+              value={message}
             ></textarea>
           </div>
           <button
@@ -49,6 +93,13 @@ const Contact = () => {
             Submit
           </button>
         </form>
+        <div className='bg-slate-100 flex flex-col'>
+          {
+            error && error.map((e, index) => 
+              <div key={index} className={`${success ? 'text-green-800' : 'text-red-600'} px-5 py-2`}>{e}</div>
+            )
+          }
+        </div>
       </main>
       <footer className="fixed bottom-0 left-0 p-8 space-x-8">
         <a className="text-white" href="/about">About</a>
