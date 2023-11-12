b632f8e5d46cd273ab56de40fccd998d4954d355
leoDonger <leodonger@icloud.com>
2023-11-11 23:48:28 -0600

    quick save

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+'use client'
+
 import React from 'react';
 import Dashboard from './dashboard/Dashboard';
+import { useState } from 'react';
+
 
 const HomePage = () => {
+  // const [showDashboard, setShowDashboard] = useState(false);
+  const [isDashboardVisible, setIsDashboardVisible] = useState(false);
+
+  const toggleDashboardVisibility = () => {
+    console.log('Toggling Dashboard', !isDashboardVisible);
+    setIsDashboardVisible(!isDashboardVisible);
+    if (!isDashboardVisible) {
+      setTimeout(() => {
+        console.log('Scrolling to Dashboard');
+        document.getElementById('dashboard')?.scrollIntoView({ behavior: 'smooth' });
+      }, 0); // Timeout ensures the DOM is updated before scrolling
+    }
+  };
+  
+  // const toggleDashboard = () => {
+  //   setShowDashboard(!showDashboard);
+  //   if (!showDashboard) {
+  //     scrollToDashboard();
+  //   }
+  // };
+
   return (
     <div className="min-h-[200vh] bg-gradient-to-br from-black via-green-200 to-black text-white">
       <header className="fixed top-0 left-0 p-8 text-xl font-bold">
@@ -10,7 +35,7 @@ const HomePage = () => {
       <nav className="fixed top-0 right-0 p-8 space-x-16">
         <a className="text-white font-bold" href="/">Home</a>
         <a className="text-white font-bold" href="/contact">Contact</a>
-        <button className="bg-black text-white px-4 py-2 rounded font-bold">Dashboard</button>
+        <button onClick={toggleDashboardVisibility} className="bg-black text-white px-4 py-2 rounded font-bold">Dashboard</button>
       </nav>
       <main className="h-screen flex flex-col justify-center items-center p-8">
         <div className="text-3xl font-semibold text-center">
@@ -19,8 +44,9 @@ const HomePage = () => {
         <p className="mt-8 text-lg text-center text-black">
           Agomotto is our internal monitoring, analysis, and policing tool for developers working on our main app, Boujè. It plays a crucial role in ensuring the safety of our platform by checking posts for explicit content and performing various safety checks.
         </p>
-        {/* dashboard goes here */}
-        <Dashboard />
+        {isDashboardVisible && (<section id='dashboard'>
+          <Dashboard />
+        </section>)}
       </main>
       {/* Add more content to make the page longer */}
       <footer className="fixed bottom-0 left-0 p-8 space-x-8">
